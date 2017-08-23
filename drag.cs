dab45558091dcb0768907789a2d0758e517ff918
aishu5 <aishwariyaramakrishnan@gmail.com>
2017-08-23 16:32:25 +0530

    Add files via upload

---
+++ drag.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class drag : MonoBehaviour {
+
+    Vector3 dist;
+    float posX;
+    float PosY;
+    void OnMouseDown()
+    {
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        PosY = Input.mousePosition.y - dist.y;
+    }
+    void OnMouseDrag()
+    {
+        Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - PosY, dist.z);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+        transform.position = worldPos;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
